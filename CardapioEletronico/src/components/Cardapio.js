7d4cd9a5aedef03601ff863bc6af3f15b9fb7a64
ThiagoLottici <thiagolottici@gmail.com>
2017-04-30 16:07:26 -0300

    Commit aplicacao exemplo para rodar o codigo

---
+++ CardapioEletronico/src/components/Cardapio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Cardapio {
+
+	render() {
+	return ( 
+		<View>
+			<Text>Cardapio!!!</Text>
+		</View>
+	)};
+};
+
+export default Cardapio;
+
